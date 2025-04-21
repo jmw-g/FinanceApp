d4a8f12a64cd9c1abaa3609de901d3a9a2767c78
jmw-g <jakegerz17@gmail.com>
2025-04-21 11:40:02 -0400

    Delete run_finance_app.sh

--- run_finance_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/Desktop/FinanceApp
-javac --module-path ~/Desktop/javafx-sdk-17.0.15/lib --add-modules javafx.controls Main.java Expense.java Login.java
-java --module-path ~/Desktop/javafx-sdk-17.0.15/lib --add-modules javafx.controls Main
